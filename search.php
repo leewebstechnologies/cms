38f9af1e438cafddbd43657d0e9d349d24d69d2f
Ali <aliuajibade317@gmail.com>
2024-12-13 01:22:42 +0100

    separate widget from sidebar through php include

--- search.php
+++ search.php
@@ -24,8 +24,7 @@
                     $post_date = $row['post_date'];
                     $post_image = $row['post_image'];
                     $post_content = $row['post_content'];
-                ?>
-               
+                ?>               
 
                 <h1 class="page-header">
                     Page Heading
